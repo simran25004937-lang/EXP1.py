8286c8ab17ed7560d16c91d77489440f2eea62bf
simran hemani <simran.25004937@jainuniversity.ac.in>
2026-09-04 10:18:15 +0530

    Add files via upload

---
+++ EXP1.py
@@ -0,0 +1,12 @@
+a = 12
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+print("Equal to:", a == b)
+print("Not Equal to:", a != b)
